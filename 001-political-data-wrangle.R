7ef615c1952f1dc10cf60e38194dd1ea86a8dc55
Andrew F. Griffin <andrewfgriffin@gmail.com>
2020-10-27 17:30:57 -0400

    Political Additions

--- 001-political-data-wrangle.R
+++ 001-political-data-wrangle.R
@@ -1,9 +1,11 @@
 library(tidyverse)
 library(stringr)
+library(lubridate)
 
 ##### PULL POLITICAL DATA ####
 slpli <- read.csv("data/slpli-and-party.csv")
 governors <- read.csv("data/gov-party.csv")
+cook_pvi <- read.csv("data/cook-pvi.csv")
 load("rda/covid.rda")
 
 ##### GOVERNORS #####
@@ -27,11 +29,41 @@ slpli <- slpli %>%
                                SLPLI == "Democrat" ~ "D"))
 save(slpli, file = "rda/slpli.rda")
 
-# BUILD POLITICAL COVID DATA SET
-covid_pol <- covid %>% 
+##### COOK PVI #####
+# ADD STATE ABBREVIATIONS
+cook_pvi <- cook_pvi %>%
+  rename(state_name = State,
+         pvi = PVI)
+cook_pvi <- covid %>% 
+  filter(date == ymd(20201001)) %>%
+  select(state, state_name) %>%
+  left_join(cook_pvi)
+cook_pvi <- cook_pvi[,c(1,3)]
+
+# FILTER NA
+cook_pvi <- cook_pvi %>%
+  filter(!is.na(pvi))
+
+cook_pvi$pvi <- str_replace(cook_pvi$pvi, "\\+", " ")
+cook_pvi <- cook_pvi %>%
+  extract(pvi, c("party", "index"), regex = "^(D|R)\\s(\\d+)") %>%
+  mutate(party = ifelse(is.na(party), "I", party),
+         index = as.numeric(index),
+         index = ifelse(is.na(index), 0, index),
+         index = ifelse(party == "D", index * (-1), index)) %>%
+  rename(pvi = index,
+         pvi_party = party)
+save(cook_pvi, file = "rda/cook_pvi.rda")
+
+##### BUILD POLITICAL COVID DATA SET #####
+covid_pol <- covid %>%
+  filter(!state == "PR") %>%
   left_join(slpli) %>% 
   left_join(governors) %>%
-  select(date, state, state_name, pop, new_cases, new_tests, new_death, hosp, percent_pos, SLPLI, party_by, slpli_party, governor_party, gov_party, mask_law, mask_date)
+  left_join(cook_pvi) %>%
+  select(date, state, state_name, pop, new_cases, new_tests, new_death, hosp, percent_pos, SLPLI, party_by, slpli_party, governor_party, gov_party, pvi, pvi_party, mask_law, mask_date)
 covid_pol
 save(covid_pol, file = "rda/covid_pol.rda")
+
+
   
--- 301-slpli-correlations.R
+++ 301-slpli-correlations.R
@@ -50,7 +50,7 @@ coefs <- tidy(model, conf.int = TRUE)
 coefs
 
 
-##### GRAPHS PER CAPITA VS POLITICAL LEANINGS STRATIFIED BY MONTH #####
+##### SLPLI -- GRAPHS PER CAPITA VS POLITICAL LEANINGS STRATIFIED BY MONTH #####
 covid_pol %>%
   filter(date >= ymd(20200301)) %>%
   mutate(month = month(date, label = TRUE)) %>%
@@ -60,64 +60,221 @@ covid_pol %>%
   ggplot(aes(x = partisan, y = cases)) +
   geom_vline(xintercept = 0, size = .5, col = "40grey") +
   geom_hline(yintercept = 0, size = .5, col = "40grey") +
-  geom_point(alpha = .5) +
+  geom_point(color = "dark blue", alpha = .5) +
   geom_smooth(method = "lm") +
   coord_cartesian(xlim = c(-50,50), ylim = c(0,NA)) +
   theme_DataStache() +
+  theme(panel.border = element_rect(colour = "black", fill=NA, size=.7)) +
   facet_wrap(. ~ month, strip.position="bottom") +
   theme(strip.text.x = element_text(size = rel(.8),
-                                    face = "bold"))
+                                    face = "bold",
+                                    margin = margin(rel(.5), rel(1), rel(.5), rel(1))),
+        strip.background = element_rect(colour = "black", size=.7)) +
+  ggtitle("Does Political Leaning Impact New Case Load") +
+  labs(caption = "Created by Andrew F. Griffin\nData The Covid Tracking Project")
+
+p_width <- 12
+p_height <- (9/16) * p_width
+
+ggsave("figs/cases-correl.png",
+       width = p_width,
+       height = p_height, 
+       dpi = "retina")
 
 covid_pol %>%
   filter(date >= ymd(20200301)) %>%
-  mutate(month = month(date)) %>%
+  mutate(month = month(date, label = TRUE)) %>%
   group_by(state, month) %>%
   summarize(death = sum(new_death) / unique(pop) * 100000,
             partisan = unique(party_by)) %>%
   ggplot(aes(x = partisan, y = death)) +
   geom_vline(xintercept = 0, size = .5, col = "40grey") +
   geom_hline(yintercept = 0, size = .5, col = "40grey") +
-  geom_point(alpha = .5) +
+  geom_point(color = "dark blue", alpha = .5) +
   geom_smooth(method = "lm") +
   coord_cartesian(xlim = c(-50,50), ylim = c(0,NA)) +
   theme_DataStache() +
+  theme(panel.border = element_rect(colour = "black", fill=NA, size=.7)) +
   facet_wrap(. ~ month, strip.position="bottom") +
   theme(strip.text.x = element_text(size = rel(.8),
-                                    face = "bold"))
+                                    face = "bold",
+                                    margin = margin(rel(.5), rel(1), rel(.5), rel(1))),
+        strip.background = element_rect(colour = "black", size=.7)) +
+  ggtitle("Does Political Leaning Impact Death Toll") +
+  labs(caption = "Created by Andrew F. Griffin\nData The Covid Tracking Project")
+
+ggsave("figs/death-correl.png",
+       width = p_width,
+       height = p_height, 
+       dpi = "retina")
 
 covid_pol %>%
   filter(date >= ymd(20200301)) %>%
-  mutate(month = month(date)) %>%
+  mutate(month = month(date, label = TRUE)) %>%
   group_by(state, month) %>%
   summarize(pos = sum(new_cases) / sum(new_tests) * 100,
             partisan = unique(party_by)) %>%
   ggplot(aes(x = partisan, y = pos)) +
   geom_vline(xintercept = 0, size = .5, col = "40grey") +
   geom_hline(yintercept = 0, size = .5, col = "40grey") +
-  geom_point(alpha = .5) +
+  geom_point(color = "dark blue", alpha = .5) +
   geom_smooth(method = "lm") +
   coord_cartesian(xlim = c(-50,50), ylim = c(0,NA)) +
   theme_DataStache() +
+  theme(panel.border = element_rect(colour = "black", fill=NA, size=.7)) +
   facet_wrap(. ~ month, strip.position="bottom") +
   theme(strip.text.x = element_text(size = rel(.8),
-                                    face = "bold"))
+                                    face = "bold",
+                                    margin = margin(rel(.5), rel(1), rel(.5), rel(1))),
+        strip.background = element_rect(colour = "black", size=.7)) +
+  ggtitle("Does Political Leaning Impact Percent Share Positive Tests") +
+  labs(caption = "Created by Andrew F. Griffin\nData The Covid Tracking Project")
+
+ggsave("figs/percent-correl.png",
+       width = p_width,
+       height = p_height, 
+       dpi = "retina")
+
 
 covid_pol %>%
   filter(date >= ymd(20200301)) %>%
-  mutate(month = month(date)) %>%
+  mutate(month = month(date, label = TRUE)) %>%
   group_by(state, month) %>%
   summarize(tests = sum(new_tests) / unique(pop) * 100000,
             partisan = unique(party_by)) %>%
   ggplot(aes(x = partisan, y = tests)) +
   geom_vline(xintercept = 0, size = .5, col = "40grey") +
   geom_hline(yintercept = 0, size = .5, col = "40grey") +
-  geom_point(alpha = .5) +
+  geom_point(color = "dark blue", alpha = .5) +
   geom_smooth(method = "lm") +
   coord_cartesian(xlim = c(-50,50), ylim = c(0,NA)) +
   theme_DataStache() +
+  theme(panel.border = element_rect(colour = "black", fill=NA, size=.7)) +
   facet_wrap(. ~ month, strip.position="bottom") +
   theme(strip.text.x = element_text(size = rel(.8),
-                                    face = "bold"))
+                                    face = "bold",
+                                    margin = margin(rel(.5), rel(1), rel(.5), rel(1))),
+        strip.background = element_rect(colour = "black", size=.7)) +
+  ggtitle("Does Political Leaning Impact New Testing") +
+  labs(caption = "Created by Andrew F. Griffin\nData The Covid Tracking Project")
+
+ggsave("figs/testing-correl.png",
+       width = p_width,
+       height = p_height, 
+       dpi = "retina") 
+
+##### COOK PVI -- GRAPHS PER CAPITA VS POLITICAL LEANINGS STRATIFIED BY MONTH #####
+covid_pol %>%
+  filter(date >= ymd(20200301)) %>%
+  mutate(month = month(date, label = TRUE)) %>%
+  group_by(state, month) %>%
+  summarize(cases = sum(new_cases) / unique(pop) * 100000,
+            partisan = unique(pvi)) %>%
+  ggplot(aes(x = partisan, y = cases)) +
+  geom_vline(xintercept = 0, size = .5, col = "40grey") +
+  geom_hline(yintercept = 0, size = .5, col = "40grey") +
+  geom_point(color = "dark blue", alpha = .5) +
+  geom_smooth(method = "lm") +
+  coord_cartesian(xlim = c(-50,50), ylim = c(0,NA)) +
+  theme_DataStache() +
+  theme(panel.border = element_rect(colour = "black", fill=NA, size=.7)) +
+  facet_wrap(. ~ month, strip.position="bottom") +
+  theme(strip.text.x = element_text(size = rel(.8),
+                                    face = "bold",
+                                    margin = margin(rel(.5), rel(1), rel(.5), rel(1))),
+        strip.background = element_rect(colour = "black", size=.7)) +
+  ggtitle("Does Political Leaning Impact New Case Load - BASED ON COOK PVI") +
+  labs(caption = "Created by Andrew F. Griffin\nData The Covid Tracking Project")
+
+p_width <- 12
+p_height <- (9/16) * p_width
+
+ggsave("figs/cases-correl-pvi.png",
+       width = p_width,
+       height = p_height, 
+       dpi = "retina")
+
+covid_pol %>%
+  filter(date >= ymd(20200301)) %>%
+  mutate(month = month(date, label = TRUE)) %>%
+  group_by(state, month) %>%
+  summarize(death = sum(new_death) / unique(pop) * 100000,
+            partisan = unique(pvi)) %>%
+  ggplot(aes(x = partisan, y = death)) +
+  geom_vline(xintercept = 0, size = .5, col = "40grey") +
+  geom_hline(yintercept = 0, size = .5, col = "40grey") +
+  geom_point(color = "dark blue", alpha = .5) +
+  geom_smooth(method = "lm") +
+  coord_cartesian(xlim = c(-50,50), ylim = c(0,NA)) +
+  theme_DataStache() +
+  theme(panel.border = element_rect(colour = "black", fill=NA, size=.7)) +
+  facet_wrap(. ~ month, strip.position="bottom") +
+  theme(strip.text.x = element_text(size = rel(.8),
+                                    face = "bold",
+                                    margin = margin(rel(.5), rel(1), rel(.5), rel(1))),
+        strip.background = element_rect(colour = "black", size=.7)) +
+  ggtitle("Does Political Leaning Impact Death Toll - BASED ON COOK PVI") +
+  labs(caption = "Created by Andrew F. Griffin\nData The Covid Tracking Project")
+
+ggsave("figs/death-correl-pvi.png",
+       width = p_width,
+       height = p_height, 
+       dpi = "retina")
+
+covid_pol %>%
+  filter(date >= ymd(20200301)) %>%
+  mutate(month = month(date, label = TRUE)) %>%
+  group_by(state, month) %>%
+  summarize(pos = sum(new_cases) / sum(new_tests) * 100,
+            partisan = unique(pvi)) %>%
+  ggplot(aes(x = partisan, y = pos)) +
+  geom_vline(xintercept = 0, size = .5, col = "40grey") +
+  geom_hline(yintercept = 0, size = .5, col = "40grey") +
+  geom_point(color = "dark blue", alpha = .5) +
+  geom_smooth(method = "lm") +
+  coord_cartesian(xlim = c(-50,50), ylim = c(0,NA)) +
+  theme_DataStache() +
+  theme(panel.border = element_rect(colour = "black", fill=NA, size=.7)) +
+  facet_wrap(. ~ month, strip.position="bottom") +
+  theme(strip.text.x = element_text(size = rel(.8),
+                                    face = "bold",
+                                    margin = margin(rel(.5), rel(1), rel(.5), rel(1))),
+        strip.background = element_rect(colour = "black", size=.7)) +
+  ggtitle("Does Political Leaning Impact Percent Share Positive Tests - BASED ON COOK PVI") +
+  labs(caption = "Created by Andrew F. Griffin\nData The Covid Tracking Project")
+
+ggsave("figs/percent-correl-pvi.png",
+       width = p_width,
+       height = p_height, 
+       dpi = "retina")
+
+
+covid_pol %>%
+  filter(date >= ymd(20200301)) %>%
+  mutate(month = month(date, label = TRUE)) %>%
+  group_by(state, month) %>%
+  summarize(tests = sum(new_tests) / unique(pop) * 100000,
+            partisan = unique(pvi)) %>%
+  ggplot(aes(x = partisan, y = tests)) +
+  geom_vline(xintercept = 0, size = .5, col = "40grey") +
+  geom_hline(yintercept = 0, size = .5, col = "40grey") +
+  geom_point(color = "dark blue", alpha = .5) +
+  geom_smooth(method = "lm") +
+  coord_cartesian(xlim = c(-50,50), ylim = c(0,NA)) +
+  theme_DataStache() +
+  theme(panel.border = element_rect(colour = "black", fill=NA, size=.7)) +
+  facet_wrap(. ~ month, strip.position="bottom") +
+  theme(strip.text.x = element_text(size = rel(.8),
+                                    face = "bold",
+                                    margin = margin(rel(.5), rel(1), rel(.5), rel(1))),
+        strip.background = element_rect(colour = "black", size=.7)) +
+  ggtitle("Does Political Leaning Impact New Testing - BASED ON COOK PVI") +
+  labs(caption = "Created by Andrew F. Griffin\nData The Covid Tracking Project")
+
+ggsave("figs/testing-correl-pvi.png",
+       width = p_width,
+       height = p_height, 
+       dpi = "retina") 
 
 
 
