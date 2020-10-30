2a48f95610cb4675fd5769ab790d02d7f613a28e
Andrew F. Griffin <andrewfgriffin@gmail.com>
2020-10-30 15:56:10 -0400

    Continued data research

--- 202-multivariet-regression.R
+++ 202-multivariet-regression.R
@@ -11,14 +11,14 @@ dat <- covid %>%
   inner_join(weather_usa) %>%
   select(1:11) %>%
   group_by(state) %>%
-  mutate(cases = rollapply(new_cases_percap, width = 14, FUN=function(x) mean(x, na.rm=TRUE), by=1, by.column=TRUE, partial=TRUE, fill=NA, align="left"),
-         ret = rollapply(retail_recreation, width = 14, FUN=function(x) mean(x, na.rm=TRUE), by=1, by.column=TRUE, partial=TRUE, fill=NA, align="right"),
-         groc = rollapply(grocery_pharmacy, width = 14, FUN=function(x) mean(x, na.rm=TRUE), by=1, by.column=TRUE, partial=TRUE, fill=NA, align="right"),
-         parks = rollapply(parks, width = 14, FUN=function(x) mean(x, na.rm=TRUE), by=1, by.column=TRUE, partial=TRUE, fill=NA, align="right"),
-         trans = rollapply(transit, width = 14, FUN=function(x) mean(x, na.rm=TRUE), by=1, by.column=TRUE, partial=TRUE, fill=NA, align="right"),
-         work = rollapply(workplace, width = 14, FUN=function(x) mean(x, na.rm=TRUE), by=1, by.column=TRUE, partial=TRUE, fill=NA, align="right"),
-         res = rollapply(residential, width = 14, FUN=function(x) mean(x, na.rm=TRUE), by=1, by.column=TRUE, partial=TRUE, fill=NA, align="right"),
-         temp = rollapply(tavg, width = 14, FUN=function(x) mean(x, na.rm=TRUE), by=1, by.column=TRUE, partial=TRUE, fill=NA, align="right"))
+  mutate(cases = rollapply(new_cases_percap, width = 7, FUN=function(x) mean(x, na.rm=TRUE), by=1, by.column=TRUE, partial=TRUE, fill=NA, align="left"),
+         ret = rollapply(retail_recreation, width = 7, FUN=function(x) mean(x, na.rm=TRUE), by=1, by.column=TRUE, partial=TRUE, fill=NA, align="right"),
+         groc = rollapply(grocery_pharmacy, width = 7, FUN=function(x) mean(x, na.rm=TRUE), by=1, by.column=TRUE, partial=TRUE, fill=NA, align="right"),
+         parks = rollapply(parks, width = 7, FUN=function(x) mean(x, na.rm=TRUE), by=1, by.column=TRUE, partial=TRUE, fill=NA, align="right"),
+         trans = rollapply(transit, width = 7, FUN=function(x) mean(x, na.rm=TRUE), by=1, by.column=TRUE, partial=TRUE, fill=NA, align="right"),
+         work = rollapply(workplace, width = 7, FUN=function(x) mean(x, na.rm=TRUE), by=1, by.column=TRUE, partial=TRUE, fill=NA, align="right"),
+         res = rollapply(residential, width = 7, FUN=function(x) mean(x, na.rm=TRUE), by=1, by.column=TRUE, partial=TRUE, fill=NA, align="right"),
+         temp = rollapply(tavg, width = 7, FUN=function(x) mean(x, na.rm=TRUE), by=1, by.column=TRUE, partial=TRUE, fill=NA, align="right"))
 
 cor <- dat %>%
   filter(!is.na(parks) & !is.na(trans) & date >= ymd(20200401)) %>%
@@ -28,7 +28,14 @@ cor <- dat %>%
             cor_trans = cor(cases, trans),
             cor_work = cor(cases, work),
             cor_res = cor(cases, res),
-            cor_temp = cor(cases, temp))
+            cor_temp = cor(cases, temp)) %>%
+  summarise(cor_ret = mean(cor_ret),
+            cor_groc = mean(cor_groc),
+            cor_parks = mean(cor_parks),
+            cor_trans = mean(cor_trans),
+            cor_work = mean(cor_work),
+            cor_res = mean(cor_res),
+            cor_temp = mean(cor_temp, na.rm = TRUE))
 cor
 
 dat %>%
@@ -36,3 +43,10 @@ dat %>%
   ggplot(aes(x = temp, y = cases)) +
   geom_point(alpha = .5) +
   geom_smooth()
+
+dat %>%
+  filter(date >= ymd(20200401)) %>%
+  ggplot(aes(x = trans, y = cases)) +
+  geom_point(alpha = .5) +
+  geom_smooth() +
+  facet_wrap(. ~ state)
