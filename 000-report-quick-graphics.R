4d4a748b0d64f34188b5455c745824860b7ccaec
Andrew F. Griffin <andrewfgriffin@gmail.com>
2020-11-05 21:55:45 -0500

    2020 11 05 Reporting

--- 000-report-quick-graphics.R
+++ 000-report-quick-graphics.R
@@ -141,21 +141,19 @@ covid_grid_cases$state <- factor(covid_grid_cases$state, levels = new_case_order
 
 p_ALL_states_new_cases_plot <- covid_grid_cases %>%
   filter(date >= ymd(20200315)) %>%
-  mutate(masks = ifelse(mask_law == "NO", "No Mask", "Yes Mask"),
-         masks = factor(masks, levels = c("Yes Mask", "No Mask"))) %>%
-  ggplot(aes(date, new_cases_percap, fill = masks)) +
+  ggplot(aes(date, new_cases_percap)) +
   geom_hline(yintercept=0, col = "grey40", size = .25) +
-  geom_bar(stat = "identity", alpha = .3, size = .1) +
+  geom_bar(stat = "identity", alpha = .3, size = .1, fill = "darkblue") +
   scale_color_fivethirtyeight() +
   scale_fill_fivethirtyeight() +
-  geom_line(aes(date, y = new_cases_percap_07da, color = masks), size = .5) +
+  geom_line(aes(date, y = new_cases_percap_07da, color = "darkblue"), size = .5) +
   xlab("Date") +
   ylab("New Cases Per 100k") +
   ggtitle("New Cases Per Capita",
           subtitle = "Ordered from most to least new cases per capita in the last 7 days") +
   labs(caption = "Created by Andrew F. Griffin, Covid Data from The Covid Tracking Project") +
   scale_x_date(date_labels = "%b", breaks= "1 month") +
-  coord_cartesian(xlim = ind_xlim_3m, ylim = c(0, 120)) +
+  coord_cartesian(xlim = ind_xlim_3m, ylim = c(0, 160)) +
   geom_hline(yintercept=0, col = "grey40", size = .4) +
   theme_DataStache() +
   theme(axis.text.x = element_text(angle=90, hjust = 1)) +
@@ -201,7 +199,7 @@ covid %>%
   xlab("Date") +
   ylab("New Cases Per 100k") +
   scale_x_date(date_labels = "%b", breaks= "1 month", expand = c(0,0)) +
-  scale_y_continuous(breaks = seq(0, 200, 10), limits = c(0, NA), expand = c(0,0)) +
+  scale_y_continuous(breaks = seq(0, 200, 20), limits = c(0, NA), expand = c(0,0)) +
   coord_cartesian(xlim = c(ymd(20200315), ind_tdy)) +
   geom_hline(yintercept=0, col = "grey40", size = .4) +
   theme_DataStache() +
@@ -242,7 +240,7 @@ covid %>%
   xlab("Date") +
   ylab("New Cases Per 100k") +
   scale_x_date(date_labels = "%b", breaks= "1 month", expand = c(0,0)) +
-  scale_y_continuous(breaks = seq(0, 200, 10), limits = c(0, NA), expand = c(0,0)) +
+  scale_y_continuous(breaks = seq(0, 200, 20), limits = c(0, NA), expand = c(0,0)) +
   coord_cartesian(xlim = c(ymd(20200315), ind_tdy)) +
   geom_hline(yintercept=0, col = "grey40", size = .4) +
   theme_DataStache() +
