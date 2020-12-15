60863150f3d3a8b0534fe2cb955968d4320b84eb
Andrew F. Griffin <andrewfgriffin@gmail.com>
2020-12-14 19:09:04 -0500

    Code simplification

--- 801-death-forecast-week.R
+++ 801-death-forecast-week.R
@@ -17,7 +17,7 @@ deaths <- covid_us_sum %>%
   select(week, day, new_death, change) 
 
 ##### MODEL GUTS #####
-dat <- deaths %>% 
+dat_mod <- deaths %>% 
   group_by(day) %>% 
   summarize(mu = mean(change),
             sd = sd(change),
@@ -25,92 +25,32 @@ dat <- deaths %>%
             se = qnorm(.975) * sd / sqrt(Ns))
 
 ##### DATES #####
-sun <- covid_us_sum %>%
-  group_by(day) %>%
-  filter(day == "Sunday" & date == max(date)) %>%
-  select(new_death, date) %>%
-  ungroup()
+day <- c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
 
-mon <- covid_us_sum %>%
-  filter(day == "Monday") %>%
-  select(new_death, date) %>%
-  slice(1) %>%
-  ungroup()
+actual <- map_df(day, function(d){
+  covid_us_sum %>%
+    group_by(day) %>%
+    filter(day == d & date == max(date)) %>%
+    select(new_death, date)
+})
 
-tues <- covid_us_sum %>%
-  filter(day == "Tuesday") %>%
-  select(new_death, date) %>%
-  slice(1) %>%
-  ungroup()
+actual <- actual %>%
+  mutate(actual = new_death) %>%
+  select(date, actual) %>%
+  ungroup
 
-wed <- covid_us_sum %>%
-  filter(day == "Wednesday") %>%
-  select(new_death, date) %>%
-  slice(1) %>%
-  ungroup()
-
-thurs <- covid_us_sum %>%
-  filter(day == "Thursday") %>%
-  select(new_death, date) %>%
-  slice(1) %>%
-  ungroup()
-
-fri <- covid_us_sum %>%
-  filter(day == "Friday") %>%
-  select(new_death, date) %>%
-  slice(1) %>%
-  ungroup()
-
-sat <- covid_us_sum %>%
-  filter(day == "Saturday") %>%
-  select(new_death, date) %>%
-  slice(1) %>%
-  ungroup()
-
-##### MODEL #####
-
-Day <- c("Mon", "Tues", "Wed", "Thurs", "Fri", "Sat")
-
-Date <- as.Date(c(ifelse(mon$date >= sun$date, mon$date, NA),
-                  ifelse(tues$date >= sun$date, tues$date, NA),
-                  ifelse(wed$date >= sun$date, wed$date, NA),
-                  ifelse(thurs$date >= sun$date, thurs$date, NA),
-                  ifelse(fri$date >= sun$date, fri$date, NA),
-                  ifelse(sat$date >= sun$date, sat$date, NA)))
-
-Model <- c(sun$new_death * dat$mu[2],
-           sun$new_death * dat$mu[3],
-           sun$new_death * dat$mu[4],
-           sun$new_death * dat$mu[5],
-           sun$new_death * dat$mu[6],
-           sun$new_death * dat$mu[7])
-
-High <- c(sun$new_death * (dat$mu[2] + dat$se[2]),
-          sun$new_death * (dat$mu[3] + dat$se[3]),
-          sun$new_death * (dat$mu[4] + dat$se[4]),
-          sun$new_death * (dat$mu[5] + dat$se[5]),
-          sun$new_death * (dat$mu[6] + dat$se[6]), 
-          sun$new_death * (dat$mu[7] + dat$se[7]))
-
-Low <- c(sun$new_death * (dat$mu[2] - dat$se[2]),
-         sun$new_death * (dat$mu[3] - dat$se[3]),
-         sun$new_death * (dat$mu[4] - dat$se[4]),
-         sun$new_death * (dat$mu[5] - dat$se[5]),
-         sun$new_death * (dat$mu[6] - dat$se[6]), 
-         sun$new_death * (dat$mu[7] - dat$se[7]))
-
-Actual <- c(ifelse(mon$date >= sun$date, mon$new_death, NA),
-            ifelse(tues$date >= sun$date, tues$new_death, NA),
-            ifelse(wed$date >= sun$date, wed$new_death, NA),
-            ifelse(thurs$date >= sun$date, thurs$new_death, NA),
-            ifelse(fri$date >= sun$date, fri$new_death, NA),
-            ifelse(sat$date >= sun$date, sat$new_death, NA))
-
-this_week <- data.frame(Day, Date, Low, Model, High, Actual) %>%
-  mutate(hit = ifelse(Actual >= Low & Actual <= High, TRUE, FALSE),
-         lo_miss = ifelse(hit == FALSE, ifelse(Actual > High, NA, (Actual - Low) / Low), NA),
-         miss = ifelse(hit == FALSE, (Actual - Model) / Model, NA),
-         hi_miss = ifelse(hit == FALSE, ifelse(Actual < Low, NA, (Actual - High) / High), NA))
+this_week <- dat_mod %>% 
+  mutate(model = actual$actual[actual$day == 'Sunday'] * mu,
+         high = actual$actual[actual$day == 'Sunday'] * (mu + se),
+         low = actual$actual[actual$day == 'Sunday'] * (mu - se)) %>%
+  left_join(actual) %>%
+  mutate(actual = ifelse(date < date[1], NA, actual),
+         date = as.Date(ifelse(date < date[1], date+7, date)),
+         hit = ifelse(actual >= low & actual <= high, TRUE, FALSE),
+         lo_miss = ifelse(hit == FALSE, ifelse(actual > high, NA, (actual - low) / low), NA),
+         miss = ifelse(hit == FALSE, (actual - model) / model, NA),
+         hi_miss = ifelse(hit == FALSE, ifelse(actual < low, NA, (actual - high) / high), NA)) %>%
+  filter(!day == 'Sunday')
 
 ##### MODEL OUTPUT #####
 library(knitr)
@@ -118,21 +58,38 @@ this_week %>%
   kable()
 
 this_week %>%
-  summarise(low_avg = mean(Low),
-            model_avg = mean(Model),
-            high_avg = mean(High),
-            low_sum = sum(Low),
-            model_sum = sum(Model),
-            high_sum = sum(High)) %>%
+  summarise(low_avg = mean(low),
+            model_avg = mean(model),
+            high_avg = mean(high),
+            low_sum = sum(low),
+            model_sum = sum(model),
+            high_sum = sum(high)) %>%
   kable()
 
+day_no_sun <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
 # Graph of Model
-this_week %>%
-  ggplot(aes(x = factor(Day, levels = c('Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun')))) +
-  geom_ribbon(aes(ymin = Low, ymax = High, group = 1), fill = 'red', alpha = .3) +
-  geom_line(aes(y = Model, group = 1), size = .6, col = 'red') +
-  geom_line(aes(y = Low, group = 1), size = .2, col = 'red') +
-  geom_line(aes(y = High, group = 1), size = .2, col = 'red') +
-  geom_point(aes(y = Actual), col = 'darkblue', size = 1) +
-  ggtitle(paste('Forecasted vs Actual Deaths: Week of ', sun$date, sep = '')) +
+P_this_week <- this_week %>%
+  ggplot(aes(x = factor(day, levels = day_no_sun))) +
+  geom_ribbon(aes(ymin = low, ymax = high, group = 1), fill = 'red', alpha = .3) +
+  geom_line(aes(y = model, group = 1), size = .4, col = 'red4') +
+  geom_line(aes(y = low, group = 1), size = .2, col = 'red4') +
+  geom_line(aes(y = high, group = 1), size = .2, col = 'red4') +
+  geom_point(aes(y = model), col = 'red4', size = .5) +
+  geom_text(aes(y = model, label = round(model)), nudge_y = 75, size = 1.7) +
+  #  geom_point(aes(y = Actual), col = 'red4', size = .5) +
+  #  ggtitle(paste('Forecasted vs Actual Deaths: Week of ', sun$date, sep = '')) +
+  ggtitle(paste('Forecasted Deaths: Week of ', this_week$date[1], sep = '')) +
+  scale_y_continuous(breaks = seq(0,7000,500)) +
+  scale_x_discrete(expand = c(.02, .02)) +
   theme_DataStache()
+
+P_this_week
+
+p_width <- 6
+p_height <- (9/16) * p_width  
+
+ggsave(paste('figs/death-forecast-week-of-', this_week$date[1], '.png', sep = ''),
+       P_this_week,
+       width = p_width,
+       height = p_height,
+       dpi = "retina")
